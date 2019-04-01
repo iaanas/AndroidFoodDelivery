4fcfa32c9d98cdd2cc284514108cd444bb1bea10
Iana Simonenko <iana.simonenko2@gmail.com>
2019-04-02 02:10:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/androidtestapp/androidfooddelivery/MainActivity.java
@@ -0,0 +1,48 @@
+package ru.androidtestapp.androidfooddelivery;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+	
+	Button btnSignIn, btnSignUp;
+	TextView txtSlogan;
+	
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+		
+		btnSignIn = findViewById( R.id.btnSignIn );
+		btnSignUp = findViewById( R.id.btnSignUp );
+		
+		txtSlogan = findViewById( R.id.txtSlogan );
+		Typeface face = Typeface.createFromAsset( getAssets(), "fonts/Nabila.ttf" );
+		txtSlogan.setTypeface( face );
+		
+		btnSignIn.setOnClickListener( new View.OnClickListener( ) {
+			@Override
+			public void onClick( View v ) {
+				
+				Intent intent = new Intent( MainActivity.this, SignIn.class );
+				startActivity( intent );
+			
+			}
+		} );
+		
+		btnSignUp.setOnClickListener( new View.OnClickListener( ) {
+			@Override
+			public void onClick( View v ) {
+				
+				Intent intent = new Intent( MainActivity.this, SignUp.class );
+				startActivity( intent );
+			
+			}
+		} );
+	}
+}
